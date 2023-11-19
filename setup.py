14e380c93c4542fb1d57bd037e85771af716dde9
Alexander Pehm <alexander@pehm.biz>
2023-11-19 10:30:57 +0100

    configured setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='spend',
+    version='0.0.1',
+    py_modules=['spend'],
+    install_requires=[
+        'Click',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spend = spend:cli',
+        ],
+    },
+)
